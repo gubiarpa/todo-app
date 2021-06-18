c4a8697022aea4a2968a332b0c8468ad37e9426f
gubiarpa <billy.arredondo@pucp.pe>
2021-06-18 02:58:28 -0500

    tuning about component

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,5 +7,10 @@
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit asperiores error minus ipsam animi? Consequuntur doloribus laudantium magni optio id quidem? Laboriosam nisi fugit reprehenderit quod incidunt, ipsam laborum totam?</p>
   </div>
 </template>
