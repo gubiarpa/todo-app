3096e1d144da4d2621ea65b78f8dd79eb31ed54b
gubiarpa <billy.arredondo@pucp.pe>
2021-06-18 23:02:58 -0500

    doneTask method

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,11 +8,13 @@
       v-for="task in tasks"
       :key="task.id"
     >
-      <v-list-item>
-        <template v-slot:default="{ active, }">
+      <v-list-item
+        @click="doneTask(task.id)"
+      >
+        <template v-slot:default>
           <v-list-item-action>
             <v-checkbox
-              :input-value="active"
+              :input-value="task.done"
               color="primary"
             ></v-checkbox>
           </v-list-item-action>
@@ -38,18 +40,27 @@
             id: 1,
             title: `Wake up`,
             subtitle: `It's the first activity in the day`,
+            done: false
           },
           {
             id: 2,
             title: `Get bananas`,
             subtitle: `Now we're going to buy some bananas`,
+            done: false
           },
           {
             id: 3,
             title: `Eat bananas`,
             subtitle: `Here we can drink a banana juice`,
+            done: false
           },
         ]
+      }
+    },
+    methods: {
+      doneTask(id) {
+        let task = this.tasks.filter(x => x.id === id)[0];
+        task.done = ! task.done;
       }
     },
   }
