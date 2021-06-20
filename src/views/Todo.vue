45f2e63eafa2d529b1887078363c6d664ba10005
gubiarpa <billy.arredondo@pucp.pe>
2021-06-19 23:42:38 -0500

    ListTasks on Component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,60 +1,7 @@
 <template>
   <div class="home pa-6">
     <field-add-task />
-    <v-list
-      v-if="$store.state.tasks.length"
-      class="pt-0"
-      flat
-    >
-    <div
-      v-for="task in $store.state.tasks"
-      :key="task.id"
-    >
-      <v-list-item
-        @click="$store.commit('doneTask', task.id)"
-      >
-        <template v-slot:default>
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="task.done"
-              color="primary"
-            ></v-checkbox>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{ 'text-decoration-line-through': task.done }"
-            >
-              {{ task.title }}
-            </v-list-item-title>
-            <v-list-item-subtitle>{{ task.subtitle }}</v-list-item-subtitle>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn
-              @click.stop="$store.commit('deleteTask', task.id)"
-              icon
-            >
-              <v-icon color="primary lighten-1">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
-    </div>
-    </v-list>
-    <div
-      v-else
-      class="no-tasks"
-    >
-      <v-icon
-        color="primary"
-        size="100"
-      >
-        mdi-check
-      </v-icon>
-      <div class="text-h5 primary--text">
-        No tasks
-      </div>
-    </div>
+    <list-tasks />
   </div>
 </template>
 
@@ -62,16 +9,8 @@
   export default {
     name: 'Home',    
     components: {
-      'field-add-task': require('@/components/Todo/FieldAddTask.vue').default
+      'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+      'list-tasks': require('@/components/Todo/ListTasks.vue').default,
     },
   }
 </script>
-
-<style lang="sass">
-  .no-tasks
-    position: absolute
-    left: 50%
-    top: 50%
-    transform: translate(-50%, -50%)
-    opacity: 0.5
-</style>
